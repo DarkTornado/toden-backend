d426378e39d097ebac52b63bffc2190b10a0eed6
Dark Tornado <dt3141592@naver.com>
2023-11-17 00:06:29 +0900

    toden filter

--- toden.js
+++ toden.js
@@ -3,7 +3,12 @@ const axios = require('axios');
 async function init() {
     var url = 'https://api-public.odpt.org/api/v4/odpt:Train?odpt:operator=odpt.Operator:Toei';
     var response = await axios.get(url);
-    return response.data;
+    var data = [];
+    response.data.forEach((e) => {
+        if (e['odpt:railway'] != 'odpt.Railway:Toei.Arakawa') return;
+        data.push(e);
+    });
+    return data;
 }
 
 module.exports = init;
