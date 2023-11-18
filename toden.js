3b99f75afef94d9f77704f4b94d2e7e1505cbc8c
Dark Tornado <dt3141592@naver.com>
2023-11-18 09:17:53 +0900

    left only necessary data

--- toden.js
+++ toden.js
@@ -6,7 +6,21 @@ async function init() {
     var data = [];
     response.data.forEach((e) => {
         if (e['odpt:railway'] != 'odpt.Railway:Toei.Arakawa') return;
-        data.push(e);
+        var stn, status;
+        if (e['odpt:toStation'] == null) {
+            stn = e['odpt:fromStation'];
+            status = 'Arrived';
+        } else {
+            stn = e['odpt:toStation'];
+            status = 'Approaching';
+        }
+        data.push({
+            no: e['odpt:trainNumber'],
+            stn: stn.split('.').at(-1),
+            status: status,
+            dest: e['odpt:destinationStation'][0].split('.').at(-1),
+            isUp: e['odpt:railDirection'] == 'odpt.RailDirection:Toei.Minowabashi'
+        });
     });
     return data;
 }
